65f7fa7fc13a6cb61ee6ef743472148d8635b448
Zhou Yu <yuzhou03@qq.com>
2017-07-06 06:06:14 +0800

    add example of sortoing `float`s

--- examples/sorting/sorting.go
+++ examples/sorting/sorting.go
@@ -9,21 +9,25 @@ import "sort"
 
 func main() {
 
-    // Sort methods are specific to the builtin type;
-    // here's an example for strings. Note that sorting is
-    // in-place, so it changes the given slice and doesn't
-    // return a new one.
-    strs := []string{"c", "a", "b"}
-    sort.Strings(strs)
-    fmt.Println("Strings:", strs)
+	// Sort methods are specific to the builtin type;
+	// here's an example for strings. Note that sorting is
+	// in-place, so it changes the given slice and doesn't
+	// return a new one.
+	strs := []string{"c", "a", "b"}
+	sort.Strings(strs)
+	fmt.Println("Strings:", strs)
 
-    // An example of sorting `int`s.
-    ints := []int{7, 2, 4}
-    sort.Ints(ints)
-    fmt.Println("Ints:   ", ints)
+	// An example of sorting `int`s.
+	ints := []int{7, 2, 4}
+	sort.Ints(ints)
+	fmt.Println("Ints:   ", ints)
 
-    // We can also use `sort` to check if a slice is
-    // already in sorted order.
-    s := sort.IntsAreSorted(ints)
-    fmt.Println("Sorted: ", s)
+	// example of sortoing `float`s.
+	floats := []float64{1.233, 4.56, -0.02}
+	sort.Float64s(floats)
+	fmt.Println(floats)
+	// We can also use `sort` to check if a slice is
+	// already in sorted order.
+	s := sort.IntsAreSorted(ints)
+	fmt.Println("Sorted: ", s)
 }
