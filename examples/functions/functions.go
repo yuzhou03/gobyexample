a71001136511f04e35f5ca1bfc24c465182cad6d
Zhou Yu <yuzhou03@qq.com>
2017-07-31 23:51:22 +0800

    add sum func

--- examples/functions/functions.go
+++ examples/functions/functions.go
@@ -9,10 +9,10 @@ import "fmt"
 // their sum as an `int`.
 func plus(a int, b int) int {
 
-    // Go requires explicit returns, i.e. it won't
-    // automatically return the value of the last
-    // expression.
-    return a + b
+	// Go requires explicit returns, i.e. it won't
+	// automatically return the value of the last
+	// expression.
+	return a + b
 }
 
 // When you have multiple consecutive parameters of
@@ -20,16 +20,49 @@ func plus(a int, b int) int {
 // like-typed parameters up to the final parameter that
 // declares the type.
 func plusPlus(a, b, c int) int {
-    return a + b + c
+	return a + b + c
+}
+
+/**
+ * 不固定参数的加法
+ */
+func sumImproved(items ... int) int {
+	var result int
+	for _, val := range items {
+		result += val
+	}
+	return result
+}
+
+/**
+ * 传递数组参数
+ */
+
+func sumArray(array [10]int) int {
+	var result int
+	for _, val := range array {
+		result += val
+	}
+
+	return result
 }
 
 func main() {
 
-    // Call a function just as you'd expect, with
-    // `name(args)`.
-    res := plus(1, 2)
-    fmt.Println("1+2 =", res)
+	// Call a function just as you'd expect, with
+	// `name(args)`.
+	p := fmt.Println
 
-    res = plusPlus(1, 2, 3)
-    fmt.Println("1+2+3 =", res)
+	res := plus(1, 2)
+	fmt.Println("1+2 =", res)
+
+	res = plusPlus(1, 2, 3)
+	fmt.Println("1+2+3 =", res)
+
+	res = sumImproved(1, 2, 3, 4, 5)
+	p("result:", res)
+
+	arr := [10] int{1, 2, 3, 4, 5}
+	p("result:", sumArray(arr))
+
 }
