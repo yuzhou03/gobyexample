1e5e2835dda4a77addae2a39e1f4a697bb42787e
Zhou Yu <yuzhou03@qq.com>
2017-09-01 23:36:16 +0800

    extract to method

--- examples/for/for.go
+++ examples/for/for.go
@@ -7,35 +7,40 @@ import "fmt"
 
 func main() {
 
-    // The most basic type, with a single condition.
-    i := 1
-    for i <= 3 {
-        fmt.Println(i)
-        i = i + 1
-    }
+	// The most basic type, with a single condition.
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
 
-    fmt.Println()
+	fmt.Println()
 
-    // A classic initial/condition/after `for` loop.
-    for j := 7; j <= 9; j++ {
-        fmt.Println(j)
-    }
+	// A classic initial/condition/after `for` loop.
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
 
-    fmt.Println()
-    // `for` without a condition will loop repeatedly
-    // until you `break` out of the loop or `return` from
-    // the enclosing function.
-    for {
-        fmt.Println("loop")
-        break
-    }
+	fmt.Println()
+	// `for` without a condition will loop repeatedly
+	// until you `break` out of the loop or `return` from
+	// the enclosing function.
+	for {
+		fmt.Println("loop")
+		break
+	}
 
-    // You can also `continue` to the next iteration of
-    // the loop.
-    for n := 0; n <= 5; n++ {
-        if n%2 == 0 {
-            continue
-        }
-        fmt.Println(n)
-    }
+	print_even_numbers()
+}
+
+
+func print_even_numbers() {
+	// You can also `continue` to the next iteration of
+	// the loop.
+	for n := 0; n <= 5; n++ {
+		if n%2 != 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
 }
